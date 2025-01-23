9295a9b9acee127839fbbb2d6f725897fdf020e2
Ashley Avena <agshelter7@gmail.com>
2025-01-23 01:41:41 +0000

    initial deploy commit

--- database/data.sql
+++ database/data.sql
@@ -9,3 +9,12 @@
 --      ('Learn to code', false),
 --      ('Build projects', false),
 --      ('Get a job', false);
+
+
+-- INSERT INTO "Users" ("username", "hashedPassword")
+-- VALUES ('ashley', 'hashedPassword123');
+
+
+insert into "Trips" ("userId", "title","description", "startDate")
+values
+(1, 'Japan trip', 'went to Tokyo and Kyoto for winter break', '2025-01-01');
--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,44 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "Users" (
+  "userId" serial PRIMARY KEY,
+  "username" text NOT NULL,
+  "hashedPassword" text NOT NULL,
+  "createdAt" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "Trips" (
+  "tripId" serial PRIMARY KEY,
+  "userId" integer NOT NULL,
+  "title" text NOT NULL,
+  "description" text,
+  "startDate" date NOT NULL,
+  "endDate" date
+);
+
+--  not null means mandatory
+
+CREATE TABLE "Photos" (
+  "photoId" serial PRIMARY KEY,
+  "tripId" integer NOT NULL,
+  "photoUrl" text NOT NULL,
+  "uploadedAt" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "Locations" (
+  "locationId" serial PRIMARY KEY,
+  "tripId" integer NOT NULL,
+  "name" text NOT NULL,
+  "latitude" text NOT NULL,
+  "longitude" text NOT NULL
+);
+
+COMMENT ON COLUMN "Trips"."description" IS 'Content of the post';
+
+ALTER TABLE "Trips" ADD FOREIGN KEY ("userId") REFERENCES "Users" ("userId");
+
+ALTER TABLE "Photos" ADD FOREIGN KEY ("tripId") REFERENCES "Trips" ("tripId");
+
+ALTER TABLE "Locations" ADD FOREIGN KEY ("tripId") REFERENCES "Trips" ("tripId");
