dbd5509f248c8d3d240bdb0f86ed7bb8dfc928cd
Ashley Avena <agshelter7@gmail.com>
2025-01-27 07:38:17 +0000

    jsx update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,29 @@ import { AuthPage } from './pages/AuthPage';
 import { NavBar } from './components/NavBar';
 import { UserProvider } from './components/UserContext';
 
-export function App() {
+import { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+// import { Router } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
+
+export default function App() {
+  const [serverData, setServerData] = useState('');
+
+  useEffect(() => {
+    async function readServerData() {
+      const resp = await fetch('/api/hello');
+      const data = await resp.json();
+
+      console.log('Data from server:', data);
+
+      setServerData(data.message);
+    }
+
+    readServerData();
+  }, []);
+
   return (
     <UserProvider>
       <Routes>
@@ -14,44 +36,17 @@ export function App() {
           <Route path="/sign-in" element={<AuthPage mode="sign-in" />} />
         </Route>
       </Routes>
+
+      <div className="logos-container">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+      <h1>{serverData}</h1>
     </UserProvider>
   );
 }
-
-// import { useEffect, useState } from 'react';
-// import reactLogo from './assets/react.svg';
-// import viteLogo from '/vite.svg';
-// import './App.css';
-// import { Router } from 'react-router-dom';
-// import { BrowserRouter } from 'react-router-dom';
-
-// export default function App() {
-//   const [serverData, setServerData] = useState('');
-
-//   useEffect(() => {
-//     async function readServerData() {
-//       const resp = await fetch('/api/hello');
-//       const data = await resp.json();
-
-//       console.log('Data from server:', data);
-
-//       setServerData(data.message);
-//     }
-
-//     readServerData();
-//   }, []);
-
-//   return (
-//     <>
-//       {/* <Router> */}
-//       <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-//         <img src={viteLogo} className="logo" alt="Vite logo" />
-//       </a>
-//       <a href="https://react.dev" target="_blank" rel="noreferrer">
-//         <img src={reactLogo} className="logo react" alt="React logo" />
-//       </a>
-//       {/* </Router> */}
-//       <h1>{serverData}</h1>
-//     </>
-//   );
-// }
