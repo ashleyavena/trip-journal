45ea00de4369157ad03596501c82d5cb4e8480eb
Ashley Avena <agshelter7@gmail.com>
2025-02-17 20:21:30 +0000

    data sql

--- database/data.sql
+++ database/data.sql
@@ -11,10 +11,10 @@
 --      ('Get a job', false);
 
 
--- INSERT INTO "Users" ("username", "hashedPassword")
--- VALUES ('ashley', 'hashedPassword123');
+INSERT INTO "Users" ("username", "hashedPassword")
+VALUES ('ashleyavena', '$argon2id$v=19$m=65536,t=3,p=4$9w90dXuMrNiZj1MKloRwiQ$TZBidljOtQjThmhWXC2/N4wqJj2smWEmu6S7pVnKbLw');
 
 
--- insert into "Trips" ("userId", "title","description", "startDate")
--- values
--- (1, 'Japan trip', 'went to Tokyo and Kyoto for winter break', '2025-01-01');
+insert into "Trips" ("userId", "title","description", "startDate")
+values
+(1, 'Japan trip', 'went to Tokyo and Kyoto for winter break', '2025-01-01');
